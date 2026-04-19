630aec06afd98a44cc9fe6c505ea8e8feaec648f
Toasted-ctrl <themickbb@gmail.com>
2026-04-19 15:41:15 +0200

    New unpack method test.

--- MCP_servers/runescape/unpack_hiscore.py
+++ MCP_servers/runescape/unpack_hiscore.py
@@ -1,3 +1,11 @@
+def unpack_skill_item(type: str, listing: list) -> dict:
+    skill_item = {}
+    if type is 'skill':
+        skill_item['type'] = 'skill'
+        skill_item['ranking'] = listing[0]
+        skill_item['level'] = listing[1]
+        skill_item['exp'] = listing[2]
+
 def unpack_hiscore(input: str) -> dict:
 
     """Unpacks the hiscore body of text returned from the RuneScape hiscore API."""
@@ -25,11 +33,9 @@ def unpack_hiscore(input: str) -> dict:
             }
 
         elif idx == 2:
-            hiscores['defence'] = {
-                'type': 'skill',
-                'ranking': listing[0],
-                'level': listing[1],
-                'exp': listing[2]
-            }
+            hiscores['defence'] = unpack_skill_item(type='skill', listing=listing)
+
+        elif idx == 3:
+            hiscores['strength'] = unpack_skill_item(type='skill', listing=listing)
 
     return hiscores
