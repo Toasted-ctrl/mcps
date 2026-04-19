dca678adaa7b6181897a956c81e9307b382fa840
Toasted-ctrl <themickbb@gmail.com>
2026-04-19 15:29:13 +0200

    Expand unpack_hiscore output.

--- MCP_servers/runescape/unpack_hiscore.py
+++ MCP_servers/runescape/unpack_hiscore.py
@@ -16,4 +16,20 @@ def unpack_hiscore(input: str) -> dict:
                 'exp': listing[2],
             }
 
+        elif idx == 1:
+            hiscores['attack'] = {
+                'type': 'skill',
+                'ranking': listing[0],
+                'level': listing[1],
+                'exp': listing[2]
+            }
+
+        elif idx == 2:
+            hiscores['defence'] = {
+                'type': 'skill',
+                'ranking': listing[0],
+                'level': listing[1],
+                'exp': listing[2]
+            }
+
     return hiscores
