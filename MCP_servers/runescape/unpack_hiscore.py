80550a2c03adfcf36a6f2bc10c7f57687d9bb294
Toasted-ctrl <themickbb@gmail.com>
2026-04-20 20:01:54 +0200

    Updated unpack_hiscore and unpack_hiscore_item.

--- MCP_servers/runescape/unpack_hiscore.py
+++ MCP_servers/runescape/unpack_hiscore.py
@@ -5,106 +5,87 @@ def unpack_hiscore_item(type: str, listing: list) -> dict:
         item['ranking'] = listing[0]
         item['level'] = listing[1]
         item['exp'] = listing[2]
+    if type == 'activity':
+        item['type'] = 'activity'
+        item['ranking'] = listing[0]
+        item['score'] = listing[1]
     return item
 
-def unpack_hiscore(input: str) -> dict:
+def unpack_hiscore(input: str) -> dict[str, dict[str, int]]:
 
     """Unpacks the hiscore body of text returned from the RuneScape hiscore API."""
+
+    hiscore_items: list = [
+        "Total Level",
+        "Attack",
+        "Defence",
+        "Strength",
+        "Constitution",
+        "Ranged",
+        "Prayer",
+        "Magic",
+        "Cooking",
+        "Woodcutting",
+        "Fletching",
+        "Fishing",
+        "Firemaking",
+        "Crafting",
+        "Smithing",
+        "Mining",
+        "Herblore",
+        "Agility",
+        "Thieving",
+        "Slayer",
+        "Farming",
+        "Runecrafting",
+        "Hunting",
+        "Construction",
+        "Summoning",
+        "Dungeoneering",
+        "Divination",
+        "Invention",
+        "Archaeology",
+        "Necromancy",
+        "Bounty Hunter",
+        "B.H. Rogues", 
+        "Dominion Tower",
+        "The Crucible",
+        "Castle Wars games",
+        "B.A. Attackers",
+        "B.A. Defenders",
+        "B.A. Collectors",
+        "B.A. Healers",
+        "Duel Tournament",
+        "Mobilising Armies",
+        "Conquest",
+        "Fist of Guthix",
+        "GG: Athletics",
+        "GG: Resource Race",
+        "WE2: Armadyl Lifetime Contribution",
+        "WE2: Bandos Lifetime Contribution",
+        "WE2: Armadyl PvP kills",
+        "WE2: Bandos PvP kills",
+        "Heist Guard Level",
+        "Heist Robber Level",
+        "CFP: 5 game average",
+        "AF15: Cow Tipping",
+        "AF15: Rats killed after the miniquest",
+        "RuneScore",
+        "Clue Scrolls Easy",
+        "Clue Scrolls Medium",
+        "Clue Scrolls Hard",
+        "Clue Scrolls Elite",
+        "Clue Scrolls Master"
+    ]
 
     hiscores = {}
     listings = input.split('\n')
 
     for idx, line in enumerate(listings):
         listing = line.split(',')
-
-        if idx == 0:
-            hiscores['total_level'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 1:
-            hiscores['attack'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 2:
-            hiscores['defence'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 3:
-            hiscores['strength'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 4:
-            hiscores['constitution'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 5:
-            hiscores['ranged'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 6:
-            hiscores['prayer'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 7:
-            hiscores['magic'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 8:
-            hiscores['cooking'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 9:
-            hiscores['woodcutting'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 10:
-            hiscores['fletching'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 11:
-            hiscores['fishing'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 12:
-            hiscores['firemaking'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 13:
-            hiscores['crafting'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 14:
-            hiscores['smithing'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 15:
-            hiscores['mining'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 16:
-            hiscores['herblore'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 17:
-            hiscores['agility'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 18:
-            hiscores['thieving'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 19:
-            hiscores['slayer'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 20:
-            hiscores['farming'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 21:
-            hiscores['runecrafting'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 22:
-            hiscores['hunting'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 23:
-            hiscores['construction'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 24:
-            hiscores['summoning'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 25:
-            hiscores['dungeoneering'] = unpack_hiscore_item(type='skill', listing=listing)
-        
-        elif idx == 26:
-            hiscores['divination'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 27:
-            hiscores['invention'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 28:
-            hiscores['archaeology'] = unpack_hiscore_item(type='skill', listing=listing)
-
-        elif idx == 29:
-            hiscores['necromancy'] = unpack_hiscore_item(type='skill', listing=listing)
+        if idx <= 29:
+            hiscores[hiscore_items[idx]] = unpack_hiscore_item(type='skill', listing=listing)
+        else:
+            hiscores[hiscore_items[idx]] = unpack_hiscore_item(type='activity', listing=listing)
 
     return hiscores
