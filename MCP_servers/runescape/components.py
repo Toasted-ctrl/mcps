761cf267d578a7f230609470d3092692241c0ef7
Toasted-ctrl <themickbb@gmail.com>
2026-04-25 15:50:01 +0200

    Fixed error handling if player_name is an empty string.

--- MCP_servers/runescape/components.py
+++ MCP_servers/runescape/components.py
@@ -8,29 +8,34 @@ from unpack_hiscore import unpack_hiscore # NOTE: Double check if this works in 
 load_dotenv()
 
 def get_player_hiscore(player_name: str) -> dict:
-    if player_name == "":
-        raise ValueError("Player name must not be None")
-    url = os.getenv(key="RUNESCAPE_HISCORE_URL")
-    params = {"player": player_name}
     try:
+        if player_name == "":
+            raise ValueError("Player name must not be None")
+        
+        url = os.getenv(key="RUNESCAPE_HISCORE_URL")
+        params = {"player": player_name}
+    
         request = requests.get(url=url, params=params)
         request.raise_for_status()
         return {
             "message": "Success",
             "detail": unpack_hiscore(input=str(request.text))
         }
+    
     except requests.exceptions.ConnectionError:
         return {
             "error": "connection_error",
             "message": "MCP server has no internet access"
         }
+    
     except requests.exceptions.HTTPError:
         return {
             "error": "http_error",
             "message": "Could not connect to RuneScape's hiscore services"
         }
-    except ValueError as verr:
+    
+    except ValueError as err:
         return {
             "error": "ValueError",
-            "messages": verr
+            "message": str(err)
         }
