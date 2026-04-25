5fccc908a29ecf019528425494d4e4fb3f31f376
Toasted-ctrl <themickbb@gmail.com>
2026-04-25 18:09:02 +0200

    Implemented test for successful request.

--- MCP_servers/runescape/tests/test_components.py
+++ MCP_servers/runescape/tests/test_components.py
@@ -2,8 +2,35 @@ from components import get_player_hiscore
 
 class TestGetPlayerHiscore:
 
-    def test_success(self):
-        pass
+    def test_success(self, monkeypatch):
+        
+        class MockResponse:
+            def __init__(self):
+                self.text = "test response"
+                self.status_code = 200
+            def raise_for_status(self):
+                pass
+
+        def mock_get(*args, **kwargs):
+            return MockResponse()
+        
+        def mock_unpack_hiscore(*args, **kwargs):
+            return {
+                "test_input": "test_output"
+            }
+        
+        monkeypatch.setattr("components.requests.get", mock_get)
+        monkeypatch.setattr("components.unpack_hiscore", mock_unpack_hiscore)
+
+        result = get_player_hiscore(player_name="test_name")
+        assert isinstance(result, dict)
+        assert result == {
+            "message": "Success",
+            "detail": {
+                "test_input": "test_output"
+            }
+        }
+
 
     def test_ValueError(self):
         result = get_player_hiscore(player_name="")
