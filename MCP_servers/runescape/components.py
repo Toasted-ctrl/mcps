51c65fb279ee4b0554c0043365aec1375ed53750
Toasted-ctrl <themickbb@gmail.com>
2026-04-26 14:14:31 +0200

    Added new tool to fetch Grand Exchange information based on item_id.

--- MCP_servers/runescape/components.py
+++ MCP_servers/runescape/components.py
@@ -14,12 +14,11 @@ def get_player_hiscore(player_name: str) -> dict:
         
         url = os.getenv(key="RUNESCAPE_HISCORE_URL")
         params = {"player": player_name}
-    
-        request = requests.get(url=url, params=params)
-        request.raise_for_status()
+        response = requests.get(url=url, params=params)
+        response.raise_for_status()
         return {
             "message": "Success",
-            "detail": unpack_hiscore(input=str(request.text))
+            "detail": unpack_hiscore(input=str(response.text))
         }
     
     except requests.exceptions.ConnectionError:
@@ -31,9 +30,41 @@ def get_player_hiscore(player_name: str) -> dict:
     except requests.exceptions.HTTPError:
         return {
             "error": "http_error",
-            "message": "Could not connect to RuneScape's hiscore services"
+            "message": "Could not connect to RuneScape's Hiscore services"
         }
     
+    except ValueError as err:
+        return {
+            "error": "ValueError",
+            "message": str(err)
+        }
+    
+def get_grand_exchange_item_id(item_id: int) -> dict:
+    try:
+        if not item_id:
+            raise ValueError("Missing item_id")
+        
+        url = os.getenv("RUNESCAPE_GE_ITEM_ID")
+        params = {"item": item_id}
+        response = requests.get(url=url, params=params)
+        response.raise_for_status()
+        return {
+            "message": "Success",
+            "item": response.json().get('item')
+        }
+    
+    except requests.exceptions.ConnectionError:
+        return {
+            "error": "connection_error",
+            "message": "MCP server has no internet access"
+        }
+    
+    except requests.exceptions.HTTPError:
+        return {
+            "error": "http_error",
+            "message": "Could not connect to RuneScape's Grand Exchange services"
+        }
+        
     except ValueError as err:
         return {
             "error": "ValueError",
--- MCP_servers/runescape/main.py
+++ MCP_servers/runescape/main.py
@@ -1,12 +1,18 @@
 from fastmcp import FastMCP
 
-from components import get_player_hiscore
+from components import get_player_hiscore, get_grand_exchange_item_id
 
 mcp = FastMCP(
-    name="RuneScape")
+    name="RuneScape MCP")
 
-@mcp.tool()
-def get_runescape_player_current_hiscore(
+@mcp.tool(
+    name="Get RuneScape Player Current Hiscore",
+    version="1.0.0",
+    meta={
+        "author": "Toasted-ctrl"
+    }
+)
+def get_current_hiscore(
     player_name: str
 ):
     """Retrieves and returns the current hiscore listings for the specified RuneScape player.
@@ -22,6 +28,42 @@ def get_runescape_player_current_hiscore(
     - A dictionary of stats / hiscores related to the specified RuneScape player."""
 
     return get_player_hiscore(player_name=player_name)
+
+@mcp.tool(
+    name="Get RuneScape Grand Exchange Item",
+    version="0.1.0",
+    meta={
+        "author": "Toasted-ctrl"
+    }
+)
+def get_grand_exchange_item(
+    item_id: int
+):
+
+    """Retrieves Grand Exhange information for an item that can be sold on the Grand Exchange.
+    
+    Only use this tool when the user is asking for sales data with an item id, or asking for Grand Exchange data on an item id.
+    If the user does not specify an item id, you may ask for one.
+    
+    Do NOT use this tool for hiscore related queries.
+    
+    Do NOT provide any arguments besides the item_id.
+    
+    Returns a dictionary which includes:
+    - The item type: str
+    - The item id: int
+    - The item name: str
+    - The item description: str
+    - Whether the item is a member's item: boolean
+    - The prcinging trends (trend, price, an optionally change if 30 days or more):
+        - Current
+        - Today
+        - day30
+        - day90
+        - day180"""
+
+    return get_grand_exchange_item_id(item_id=item_id)
+
 
 if __name__ == "__main__":
     mcp.run(
