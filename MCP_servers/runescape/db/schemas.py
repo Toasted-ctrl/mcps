475be56067bb304da95a4f5e480584e15031a6ed
Toasted-ctrl <themickbb@gmail.com>
2026-05-23 21:47:41 +0200

    Added new schema for tracked hiscores.

--- MCP_servers/runescape/db/schemas.py
+++ MCP_servers/runescape/db/schemas.py
@@ -1,7 +1,7 @@
 import uuid
 
 from datetime import datetime
-from sqlalchemy import String, func, DateTime, text, UUID, Boolean
+from sqlalchemy import String, func, DateTime, text, UUID, Boolean, Integer, BigInteger
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -36,5 +36,71 @@ class ProdTrackedUsers(Base):
 
     is_active: Mapped[bool] = mapped_column(
         Boolean,
+        nullable=False
+    )
+
+class StageHiscores_1(Base):
+    __tablename__ = "stg_hiscores_1"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        primary_key=True,
+        server_default=text("gen_random_uuid()")
+    )
+
+    ingest_item_id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        nullable=False
+    )
+
+    ingest_source_id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        nullable=False
+    )
+
+    ingest_date: Mapped[datetime] = mapped_column(
+        DateTime(timezone=False),
+        nullable=False
+    )
+
+    source_id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        nullable=False
+    )
+
+    type: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False
+    )
+
+    inserted_date: Mapped[datetime] = mapped_column(
+        DateTime(timezone=False),
+        nullable=False,
+        server_default=func.now()
+    )
+
+    inserted_by: Mapped[str] = mapped_column(
+        String(40),
+        nullable=False,
+        server_default=text("current_user")
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
+
+    rank: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False
+    )
+
+    level: Mapped[int | None] = mapped_column(
+        Integer,
+        nullable=True
+    )
+
+    points: Mapped[int] = mapped_column(
+        BigInteger,
         nullable=False
     )
