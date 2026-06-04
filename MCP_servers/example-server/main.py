e35951309e515d8827d403d271d4ddd7c0d8d5b8
Mick <themickbb@gmail.com>
2026-06-04 21:07:49 +0200

    Port configuration through dotenv.

--- MCP_servers/example-server/main.py
+++ MCP_servers/example-server/main.py
@@ -1,5 +1,7 @@
+from dotenv import load_dotenv
 from fastmcp import FastMCP
 from prometheus_client import start_http_server
+import os
 import sys
 
 from logger.logger import get_logger
@@ -17,20 +19,28 @@ mcp = FastMCP(
 )
 @metrics_handler
 def add(num_1: int, num_2: int) -> dict[str, int]:
-    log.info(f"Calling '{sys._getframe().f_code.co_name}' with args: {num_1}, {num_2}")
     return {
         "answer": num_1 + num_2
     }
 
 if __name__ == "__main__":
-    log.info("Starting Prometheus endpoint at port '8787'")
+    log.info("Loading environment variables")
+    load_dotenv()
+    PROMETHEUS_PORT = int(os.getenv("PROMETHEUS_PORT"))
+    if PROMETHEUS_PORT is None:
+        sys.exit("Missing Prometheus port configuration")
+    MCP_PORT = int(os.getenv("MCP_PORT"))
+    if MCP_PORT is None:
+        sys.exit("Missing MCP port configuration")
+
+    log.info(f"Starting Prometheus endpoint at port {PROMETHEUS_PORT}")
     start_http_server(
-        port=8787,
+        port=PROMETHEUS_PORT,
         addr="0.0.0.0"
     )
     mcp.run(
         transport="streamable-http",
         host="0.0.0.0",
-        port=8000,
+        port=MCP_PORT,
         path="/mcp"
     )
