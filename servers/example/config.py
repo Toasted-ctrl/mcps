13f2949f76b2c5de8d4fe37b518b7b41dd316538
Mick <themickbb@gmail.com>
2026-06-13 15:47:47 +0200

    Updated config.

--- servers/example/config.py
+++ servers/example/config.py
@@ -1,15 +1,30 @@
 from dotenv import load_dotenv
 import os
+import sys
 
 from shared.logger import get_logger
 
-log = get_logger()
-
 class MCPConfig:
-    log.info("Loading environment variables")
-    load_dotenv()
-    PROMETHEUS_PORT: int = int(os.getenv("PROMETHEUS_PORT"))
-    MCP_PORT: int = int(os.getenv("MCP_PORT"))
-    MCP_NAME: str = os.getenv("MCP_NAME")
+
+    REQUIRED_VARS = [
+        "MCP_NAME",
+        "MCP_PORT",
+        "PROMETHEUS_PORT"
+    ]
+
+    def __init__(self):
+        load_dotenv()
+        missing = [var for var in self.REQUIRED_VARS if not os.getenv(var)]
+        if missing:
+            sys.exit(f"Missing environment variables {', '.join(missing)}")
+
+        log = get_logger(name=os.getenv("MCP_NAME"))
+        log.info("Setting up config")
+
+        self.PROMETHEUS_PORT: int = int(os.getenv("PROMETHEUS_PORT"))
+        self.MCP_PORT: int = int(os.getenv("MCP_PORT"))
+        self.MCP_NAME: str = os.getenv("MCP_NAME")
+
+        log.info("Finished setting up config")
 
 config = MCPConfig()
