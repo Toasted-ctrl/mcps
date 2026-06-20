58dcbc1979453c4c787722e0a73a514a9de9f8bc
Mick <themickbb@gmail.com>
2026-06-20 22:27:31 +0200

    Implemented get_database_names and get_table_names.

--- servers/data_explorer/main.py
+++ servers/data_explorer/main.py
@@ -2,6 +2,9 @@ from prometheus_client import start_http_server
 
 from .config import config
 from .server import mcp
+from .tools.database.get_database_names import get_database_names
+from .tools.tables.get_table_names import get_table_names
+
 
 from shared.logger import get_logger
 
