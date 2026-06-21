27ed6c1c1ebf45a9c939e1d22b084c1bbae1cfca
Mick <themickbb@gmail.com>
2026-06-21 11:40:50 +0200

    Removed double whiteline.

--- servers/data_explorer/main.py
+++ servers/data_explorer/main.py
@@ -6,7 +6,6 @@ from .tools.database.get_database_names import get_database_names
 from .tools.tables.get_table_details import get_table_details
 from .tools.tables.get_table_names import get_table_names
 
-
 from shared.logger import get_logger
 
 if __name__ == "__main__":
