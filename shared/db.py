1ab5cf7daf4b0516ee75cc33cb9993e4fd80701d
Mick <themickbb@gmail.com>
2026-08-18 17:57:06 +0200

    Iterator deprecated, replaced with Generator.

--- shared/db.py
+++ shared/db.py
@@ -1,10 +1,20 @@
-from collections.abc import Iterator
+from collections.abc import Generator
 from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
+from .logger import get_logger
+
+log = get_logger()
+
+
 @contextmanager
-def get_db_session(db_url: str) -> Iterator[Session]:
+def get_db_session(db_url: str) -> Generator[Session]:
+    """Function that will yield a database session.
+    Rollback will happen automatically upon detection of issues.
+    Does not autocommit or autoflush, committing and flushing will
+    require manual actions."""
+
     engine = create_engine(url=db_url, echo=False)
     SessionLocal = sessionmaker(
         bind=engine,
